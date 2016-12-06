c34555466c86ca026c1f2266ff1b926934da901e
zonesan <san@zoobook.org>
2016-12-06 16:52:07 +0800

    roleadd package method.

--- pkg/openshift/client.go
+++ pkg/openshift/client.go
@@ -7,6 +7,7 @@ import (
 	projectapi "github.com/openshift/origin/pkg/project/api/v1"
 	rolebindingapi "github.com/openshift/origin/pkg/rolebinding/api/v1"
 	"github.com/zonesan/clog"
+	kapi "k8s.io/kubernetes/pkg/api/v1"
 )
 
 type OClient struct {
@@ -36,8 +37,8 @@ func (oc *OClient) CreateProject(r *http.Request, name string) (*projectapi.Proj
 	{
 		projRequest.DisplayName = name
 		projRequest.Name = oc.user + "-org-" + genRandomName(8)
-		projRequest.Annotations = make(map[string]string)
-		projRequest.Annotations["datafoundry.io/requester"] = oc.user
+		// projRequest.Annotations = make(map[string]string)
+		// projRequest.Annotations["datafoundry.io/requester"] = oc.user
 	}
 
 	proj := new(projectapi.Project)
@@ -57,6 +58,11 @@ func (oc *OClient) ListRoles(r *http.Request, project string) (*rolebindingapi.R
 	roles := new(rolebindingapi.RoleBindingList)
 
 	oc.client.OGet(uri, roles)
+
+	if oc.client.Err != nil {
+		clog.Error(oc.client.Err)
+		return nil, oc.client.Err
+	}
 	//clog.Debug(roles)
 
 	rolesResult := new(rolebindingapi.RoleBindingList)
@@ -76,10 +82,51 @@ func (oc *OClient) ListRoles(r *http.Request, project string) (*rolebindingapi.R
 	return rolesResult, nil
 }
 
-func (oc *OClient) RoleAdd(r *http.Request, project, name string, admin bool) error {
-	return nil
+func (oc *OClient) RoleAdd(r *http.Request, project, name string, admin bool) (*rolebindingapi.RoleBinding, error) {
+
+	roles, err := oc.ListRoles(r, project)
+	if err != nil {
+		clog.Error(err)
+		return nil, err
+	}
+	roleRef := "edit"
+	if admin {
+		roleRef = "admin"
+	}
+	role := findRole(roles, roleRef)
+	create := false
+
+	if role == nil { //post else put
+		create = true
+		role = new(rolebindingapi.RoleBinding)
+		role.Name = roleRef
+	}
+
+	subject := kapi.ObjectReference{Kind: "User", Name: name}
+	role.Subjects = append(role.Subjects, subject)
+	role.UserNames = append(role.UserNames, name)
+
+	uri := fmt.Sprintf("/projects/%v/rolebindings", project)
+
+	if create {
+		oc.client.OPost(uri, role, role)
+	} else {
+		uri += "/" + roleRef
+		oc.client.OPut(uri, role, role)
+	}
+
+	return role, oc.client.Err
 }
 
 func (oc *OClient) RoleRemove(r *http.Request, project, name string) error {
+	return nil
+}
+
+func findRole(roles *rolebindingapi.RoleBindingList, roleRef string) *rolebindingapi.RoleBinding {
+	for _, role := range roles.Items {
+		if role.RoleRef.Name == roleRef {
+			return &role
+		}
+	}
 	return nil
 }
